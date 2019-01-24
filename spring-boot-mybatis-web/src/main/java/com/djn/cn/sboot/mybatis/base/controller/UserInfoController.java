bbafbee93cf41a6f8c0e0a5dbb76bd456d5256d4
NieDongjia <327803131@qq.com>
2019-01-24 23:07:56 +0800

    删除无用代码

--- spring-boot-mybatis-web/src/main/java/com/djn/cn/sboot/mybatis/base/controller/UserInfoController.java
+++ spring-boot-mybatis-web/src/main/java/com/djn/cn/sboot/mybatis/base/controller/UserInfoController.java
@@ -1,12 +1,10 @@
 package com.djn.cn.sboot.mybatis.base.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.djn.cn.sboot.mybatis.base.constants.RedisCacheConstants;
-import com.djn.cn.sboot.mybatis.base.service.IUserInfoService;
 import com.djn.cn.sboot.mybatis.base.util.CacheClient;
 import com.djn.cn.sboot.mybatis.base.util.CacheClientUtilFactory;
 
@@ -16,12 +14,9 @@ import com.djn.cn.sboot.mybatis.base.util.CacheClientUtilFactory;
 
 @Controller
 public class UserInfoController  {
-    @Autowired
-    private  IUserInfoService  iUserInfoService;
     @ResponseBody
     @RequestMapping(value = "/")
     String home() {   
-    	System.out.println(iUserInfoService.listAll().size());
 		CacheClient singleRedisCacheClient = CacheClientUtilFactory.createClient(1000);
         return singleRedisCacheClient.getCache(RedisCacheConstants.CACHE_KEY_MUDLE_NAME+"test1");
     }

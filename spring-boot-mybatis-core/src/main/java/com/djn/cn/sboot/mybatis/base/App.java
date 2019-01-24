199fd842bedb9601b5872238ce4357725127b226
NieDongjia <327803131@qq.com>
2019-01-24 21:34:54 +0800

    初始化项目

---
+++ spring-boot-mybatis-core/src/main/java/com/djn/cn/sboot/mybatis/base/App.java
@@ -0,0 +1,13 @@
+package com.djn.cn.sboot.mybatis.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
